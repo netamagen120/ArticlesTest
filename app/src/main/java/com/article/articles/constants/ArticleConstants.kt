cdf938af26b94b8f479f3de81554a507cc8dc886
netamagen120 <neta.magen8@gmail.com>
2025-08-21 12:29:07 +0300

    add list with articles

---
+++ app/src/main/java/com/article/articles/constants/ArticleConstants.kt
@@ -0,0 +1,7 @@
+package com.article.articles.constants
+
+object ArticleConstants {
+    const val NEWS_URL = "https://newsapi.org/"
+    const val API_KEY = "d369f4e686c1452a8df27704b4446ac1"
+    const val BBC_NEWS = "bbc-news"
+}
